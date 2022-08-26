1f699aae6237791f1c0209b651444bba557e8d63
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2022-08-26 11:52:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function alterarImagem(avatar) {
   let id = document.getElementById('avatar');
   if ((avatar = id)) {
     document.getElementById('avatar').src =
-      'https://api.qrserver.com/v1/create-qr-code/?size=240x240&data=https://atilamathiaslink.vercel.app/';
+      'img/QRCode.png';
     document.getElementById('avatar').id = 'qrcode';
   } else {
     document.getElementById('qrcode').src = 'img/AtilaAvatar.png';
